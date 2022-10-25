3c3bf1335fe8c607aee360281d46b4ccf5cd3219
Sameer Maurya <sameer.apsdk@gmail.com>
2022-10-25 16:15:48 +0530

    added slider code

---
+++ slider.py
@@ -0,0 +1,11 @@
+import gradio as gr
+def start(name, morning_is, temp):
+            msg = "Good morning" if morning_is else "Good evening"
+            greeting = "%s %s. It is %s degrees today" % (msg, name, temp)
+            cels = (temp - 32) * 5 / 9
+            return greeting, round(cels, 2)
+
+face = gr.Interface(fn=start, inputs=["text", "checkbox",
+                                     gr.inputs.Slider(0, 100)],
+                                    outputs=["text", "number"])
+face.launch()
